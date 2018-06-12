7dc67b596862e091ab8325ee232b100ecda7d0c0
Sunny Miglani <sm15504@my.bristol.ac.uk>
2018-06-13 00:26:48 +0100

    adding comprehend stuff

--- lambda_data/mainPythonFile.py
+++ lambda_data/mainPythonFile.py
@@ -1,28 +1,26 @@
-# import json
-# import boto3
+import json;
+import boto3;
 import sys;
-print("made it to json! ");
 
-print("made it to boto3! ");
-print("communication from python! " );
+print("communication from python!");
+
+comprehend = boto3.client(service_name='comprehend', region_name='eu-west-1');
+textFile = open("much_ado_about_nothing.txt", "r", encoding="utf8");
+text = textFile.read();
+
+
+
+# print('Calling DetectEntities')
+stringResponse = json.dumps(comprehend.detect_entities(Text=text, LanguageCode='en'), sort_keys=True, indent=4) # type string
+dictResponse = json.loads(stringResponse);
+listOfEntities = [];
+listOfDefines = [];
+for entity in dictResponse['Entities']:
+    listOfEntities.append(entity['Text']);
+    listOfDefines.append(entity['Type']);
+    if(entity['Type']=="PERSON"): print(entity['Text']);
+
+
 sys.stdout.flush()
-# comprehend = boto3.client(service_name='comprehend', region_name='eu-west-1');
-# textFile = open("much_ado_about_nothing.txt", "r", encoding="utf8");
-# text = textFile.read();
-
-
-
-# # print('Calling DetectEntities')
-# stringResponse = json.dumps(comprehend.detect_entities(Text=text, LanguageCode='en'), sort_keys=True, indent=4) # type string
-# dictResponse = json.loads(stringResponse);
-# listOfEntities = [];
-# listOfDefines = [];
-# for entity in dictResponse['Entities']:
-#     listOfEntities.append(entity['Text']);
-#     listOfDefines.append(entity['Type']);
-#     if(entity['Type']=="PERSON"): print(entity['Text']);
-#     # print("Entity : {0}, Type : {1}".format(entity['Text'], entity['Type']));
-
-
 # print('End of DetectEntities\n')
 
