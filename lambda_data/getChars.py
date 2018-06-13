a4633ddc688c1775f7249d917166db7ee23c5c76
Sunny Miglani <sm15504@my.bristol.ac.uk>
2018-06-13 15:57:51 +0100

    forgot to add getChars

---
+++ lambda_data/getChars.py
@@ -0,0 +1,29 @@
+import json;
+import boto3;
+import sys;
+import os;
+configPath = os.environ['LAMBDA_TASK_ROOT']+"/much_ado_about_nothing.txt";
+
+comprehend = boto3.client(service_name='comprehend', region_name='eu-west-1');
+
+textFile = open(configPath, "r");
+text = (textFile.read()).decode("utf-8");
+
+
+stringResponse = json.dumps(comprehend.detect_entities(Text=text, LanguageCode='en'), sort_keys=True, indent=4) # type string
+dictResponse = json.loads(stringResponse);
+listOfEntities = [];
+for entity in dictResponse['Entities']:
+    input_text = entity['Text'].lower();
+    input_type = entity['Type'].lower();
+    if(input_type == "person"):
+        if(input_text not in listOfEntities):
+            listOfEntities.append(input_text);
+        else:
+            continue;
+
+for person in listOfEntities:
+    print(person+",");
+
+sys.stdout.flush()
+
